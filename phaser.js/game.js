1415db985fab00dd3b56b52c202ec07071c807eb
Smarty Pants <amcurrent@hotmail.com>
2019-12-03 18:25:33 -0600

    created moving circle

---
+++ phaser.js/game.js
@@ -0,0 +1,29 @@
+// Define "gameState" here
+const gameState = {};
+
+function create() {
+  gameState.circle = this.add.circle(20, 20, 5, 0xff0000);
+  
+  // Create a circle and assign it to gameState.circle here
+
+}
+
+function update() {
+  // Update the circle in gameState.circle here
+ gameState.circle.y += 1;
+   gameState.circle.x += 1;
+
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 600,
+	backgroundColor: "#99ff99",
+	scene: {
+    create,
+    update
+	}
+}
+
+const game = new Phaser.Game(config)
